6d21a361417f2c2ef0d6d307e0a279c567981882
adimaniac <adityabhigade31@gmail.com>
2023-09-23 02:05:55 +0530

    add notebook files

--- src/App.js
+++ src/App.js
@@ -22,9 +22,9 @@ function App() {
 
   return (
     <div className="App">
-      {/* <Navbar />
-    {output} */}
-    <PrakritiReport />
+      <Navbar />
+    {output}
+    {/* <PrakritiReport /> */}
     </div>
   );
 }
