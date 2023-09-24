143ba3b1cbadaf439b86e71a787d83504cffc18b
Adit Shriyans <shriyansadit@gmail.com>
2023-09-24 22:39:43 +0530

    display PrakritiReport

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   let [appState, setAppState] = useState("welcome");
   let [results, setResults] = useState([]);
   const [outputs, setOutputs] = useState({});
-  const [responseValue, setResponseValue] = useState(null);
+  const [prediction, setPrediction] = useState();
   let output;
 
   if (appState === "welcome") {
@@ -20,7 +20,7 @@ function App() {
   } else if (appState === "questions") {
     output = <Questions setAppState={setAppState} setResults={setResults} />;
   } else {
-    output = <PrakritiReport prakriti={"Vata"} outputs={outputs} content={"Dori me Interimo, adapare Dori me Ameno, Ameno Latire Latiremo Dori me Ameno Omenare imperavi ameno Dimere, dimere matiro Matiremo Ameno Omenare imperavi emulari, ameno Omenare imperavi emulari, ameno"} />;
+    output = <PrakritiReport prediction={prediction} />;
   }
 
   for (let i = 0; i < results.length; i++) {
@@ -29,80 +29,23 @@ function App() {
 
   const jsonString = JSON.stringify(outputs);
 
-  console.log(jsonString);
-
-  // Send the POST request to your server
-  // axios
-  // .post('http://localhost:8000/', jsonString, {
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //   },
-  // })
-  // .then((response) => {
-  //   // Handle the response from the server
-  //   setResponseValue(response.data); // Assuming the response is an integer
-  // })
-  // .catch((error) => {
-  //   console.error('Error sending data:', error);
-  // });
-  // fetch('http://localhost:8000', {
-  //   method: 'POST',
-  //   mode: 'cors',
-  //   body: jsonString
-  // })
-  // .then((response) => {
-  //   if (!response.ok) {
-  //     throw new Error('Network response was not ok');
-  //   }
-  //   return response.json(); // Assuming the response is JSON
-  // })
-  // .then((data) => {
-  //   // Handle the response data here
-  //   setResponseValue(data);
-  // })
-  // .catch((error) => {
-  //   console.error('Error:', error);
-  // });
-
-
-  // axios.post("http://localhost:8000", (
-  //   jsonString
-  // ))
-  // .then(res => (
-  //   console.log(res.data)
-  // ))
-  // .catch((error) => {
-  //     console.error('Error:', error);
-  //   });
-  if(jsonString.length>2){
-  axios
-  .post("https://prakriti.onrender.com", jsonString, {
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
-  .then((response) => {
-    console.log("Response from server:", response.data);
-    // Assuming the response is an integer, you can use setResponseValue if needed
-    // setResponseValue(response.data);
-  })
-  .catch((error) => {
-    console.error("Error sending data:", error);
-  });
-}
-
-
-    // fetch("http://localhost:8000",{
-    //   method: "POST",
-    //   headers: {
-    //   'Content-Type': 'application/json',
-    // },
-    //   body: jsonString
-    // })
-    // .catch((error) => {
-    //   console.error('Error:', error);
-    // });
-
+  // "https://prakriti.onrender.com"
+  // "http://localhost:8000"
+  if (jsonString.length > 2) {
+    axios
+      .post("http://localhost:8000", jsonString, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then((response) => {
+        console.log("Response from server:", response.data);
+        setPrediction(response.data.prediction);
+      })
+      .catch((error) => {
+        console.error("Error sending data:", error);
+      });
+  }
 
   return (
     <div className="App">
--- src/PrakritiReport.js
+++ src/PrakritiReport.js
@@ -4,36 +4,43 @@ import "./PrakritiReport.css"
 import PrakritiBorder from "./assets/dec.png"
 import Details from './constants/Details';
 
-const PrakritiReport = ({prakriti,content}) => {
-  return (
-    <div>
-        {/* <Navbar /> */}
-        <section className='Report'>
-            <div className='PrakritiContainer'>
-                 <img src={PrakritiBorder} className='PrakritiBorder'></img>   
-                 <h1 className='PrakritiName'>{prakriti}</h1>
-            </div>
-            <div className='Report-details-div'>
-                <p className='Report-details-text'>
-                    {content}
-                </p>
-            </div>
-            <div className='Recommendation-div'>
-                <h1 className='Recommendation-heading'>Recommendations:</h1>
-                <div className='Plans'>
-                    <button className='Diet-btn btn'>Diet Plannig</button>
-                    <button className='Life-btn btn'>Lifestyle Planning</button>
+const PrakritiReport = (props) => {
+    const prakritiOrder = ["Kapha", "Pitta", "Vata"];
+    const pr = prakritiOrder[props.prediction]
+    console.log(Details[pr]);
+    const prakriti = Details[pr]?Details[pr].name:"Error loading Prakriti";
+    const content = Details[pr]?Details[pr].content:"";
+    // const prakriti = "Vata";
+    // const content = "abcs";
+    return (
+        <div>
+            {/* <Navbar /> */}
+            <section className='Report'>
+                <div className='PrakritiContainer'>
+                    <img src={PrakritiBorder} className='PrakritiBorder'></img>
+                    <h1 className='PrakritiName'>{prakriti}</h1>
                 </div>
-                <input 
-                type="text" 
-                placeholder="Type what you want to know!" 
-                className='recommendation-input'
-                >
-                </input>
-            </div>
-        </section>
-    </div>
-  )
+                <div className='Report-details-div'>
+                    <p className='Report-details-text'>
+                        {content}
+                    </p>
+                </div>
+                <div className='Recommendation-div'>
+                    <h1 className='Recommendation-heading'>Recommendations:</h1>
+                    <div className='Plans'>
+                        <button className='Diet-btn btn'>Diet Plannig</button>
+                        <button className='Life-btn btn'>Lifestyle Planning</button>
+                    </div>
+                    <input
+                        type="text"
+                        placeholder="Type what you want to know!"
+                        className='recommendation-input'
+                    >
+                    </input>
+                </div>
+            </section>
+        </div>
+    )
 }
 
 export default PrakritiReport
